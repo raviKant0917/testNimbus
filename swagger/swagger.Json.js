a6fe4b8bc3ad8629e0c24593265cd27f587a8eb5
ravikant0917 <21bcs108@nith.ac.in>
2023-02-16 19:16:17 +0530

    api docs error fixed

---
+++ swagger/swagger.Json.js
@@ -0,0 +1,28 @@
+import swaggerJSDoc from "swagger-jsdoc";
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: "Nimbus Backend",
+            version: "1.0.0",
+            description: "Nimbus backend api",
+            license: {
+                name: 'Licensed Under MIT',
+                url: 'https://spdx.org/licenses/MIT.html',
+            },
+            contact: {
+                name: 'JSONPlaceholder',
+                url: 'https://jsonplaceholder.typicode.com',
+            },
+        },
+        servers: [
+            {
+                 url: 'https://test4-deh4.onrender.com',
+                 description: 'Development server',
+            },
+        ],
+    },
+    apis: ["./swagger/*.js"]
+}
+export const specs = swaggerJSDoc(options)
