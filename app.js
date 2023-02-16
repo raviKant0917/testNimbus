b2cf7270d991359d654668b5d8ecedcc9fd3e05d
ravikant0917 <21bcs108@nith.ac.in>
2023-02-16 13:40:53 +0530

    swagger docs added

--- app.js
+++ app.js
@@ -4,26 +4,7 @@ import mongoose from 'mongoose';
 import Connection from './Database/db.js';
 import Router from './routes/route.js';
 import swaggerUi from 'swagger-ui-express';
-import swaggerJSDoc from 'swagger-jsdoc';
-const options = {
-    definition:{
-        openai: "3.0.0",
-        info:{
-            title: "Nimbus Backend",
-            version: "1.0.0",
-            description: "Nimbus backend api"
-        },
-        servers: [
-            {
-                url: "http://localhost:8080",
-                
-            },
-        ],
-    },
-    apis: ["./controllers/*.js"],
-}
-
-const specs = swaggerJSDoc(options)
+import {specs} from './swagger.Json.js';
 
 
 mongoose.set('strictQuery', false);
