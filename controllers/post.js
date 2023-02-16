f10866d9d41d70819eb1ebd3bc1b4b4e5b739a77
mukesh <mukesh1150935@gmail.com>
2023-02-16 23:26:39 +0530

    postfeed updated like comment added

--- controllers/post.js
+++ controllers/post.js
@@ -1,86 +1,120 @@
 import Postmodel from "../models/post.js";
 import router from "../routes/route.js";
+import { MongoClient, ObjectId } from "mongodb";
 
+//getting all posts
+export const getPost = async (req, res) => {
+  try {
+    const postMessages = await Postmodel.find();
+    // console.log(postMessages);
+    res.status(200).json(postMessages);
+  } catch (e) {
+    res.status(404).json({ message: e.message });
+  }
+};
 
-export const getPost=async (req,res)=>{
-    try{
-        const postMessages=await Postmodel.find()
-        // console.log(postMessages);
-        res.status(200).json(postMessages);
+//creating a post
+export const createPost = async (req, res) => {
+  const post = req.body;
+  const newPost = new Postmodel(post);
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+};
 
+// updating a post
+export const updatePost = async (req, res) => {
+  const id = req.params;
+  // const post=await Postmodel.findById(id);
+  const post = req.body;
 
-    }catch(e){
-        res.status(404).json({message:e.message})
+  try {
+    const updatedPost = await Postmodel.findByIdAndUpdate({ _id: id }, post, {
+      new: true,
+    });
+
+    res.status(200).json(updatePost);
+  } catch (e) {
+    res.status(404).json({ message: e.message });
+  }
+};
+
+export const deletePost = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    await Postmodel.findByIdAndDelete(id);
+    res.status(201).json({ message: "post deleted" });
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+};
+
+//like and unlike a particular post
+export const likePost = async (req, res) => {
+  const id = req.params.id;
+  const user = req.body.id;
+
+  const post = await Postmodel.findById(id);
+  if (!post) {
+    res.status(501).json({ message: "post not found" });
+  } else {
+    console.log(post);
+
+    try {
+      const liked = post.likes.includes(user);
+
+      if (liked) {
+        await Postmodel.findByIdAndUpdate(
+          id,
+          { $pull: { likes: user },likeCount:post.likeCount-1 },
+          {
+            new: true,
+          }
+        );
+        res.status(200).json({ message: "Post unliked" });
+      } else {
+        await Postmodel.findByIdAndUpdate(
+          id,
+          { $push: { likes: req.body.id },likeCount:post.likeCount+1 },
+          {
+            new: true,
+          }
+        );
+        res.status(200).json({ message: "Post liked" });
+      }
+    } catch (e) {
+      res.status(500).json({ message: e.message });
     }
-}
+  }
+};
 
-export const createPost=async(req,res)=>{
-    const post=req.body;
-    const newPost =new Postmodel(post);
-    try{
-          await newPost.save();
-          res.status(201).json(newPost);
+// adding a comment
+export const addComment = async (req, res) => {
+  const id = req.params.id;
+  const comment = {
+    text: req.body.message,
+    postedBy: req.body.id,
+  };
+  try {
+    await Postmodel.findByIdAndUpdate(
+      id,
+      {
+        $push: {
+          comments: comment,
+        },
+      },
+      {
+        new: true,
+      }
+    );
 
-    }catch(e){
-        res.status(500).json({message:e.message})
-
-    }
-
-}
-
-export const updatePost=async(req,res)=>{
-    const id=req.params
-    // const post=await Postmodel.findById(id);
-    const post=req.body;
-    
-    try{
-
-       const updatedPost=await Postmodel.findByIdAndUpdate(
-            {_id:id},
-            post,
-            {new:true}
-        )
-
-        
-      res.status(200).json(updatePost);
-
-    }catch(e){
-        res.status(404).json({message:e.message})
-
-    }
-
-}
-
-export const deletePost=async(req,res)=>{
-    const id=req.params.id;
-    
-    try{
-          await Postmodel.findByIdAndDelete(id);
-          res.status(201).json({message:'post deleted'});
-
-    }catch(e){
-        res.status(500).json({message:e.message})
-
-    }
-
-}
-
-export const likePost=async(req,res)=>{
-    const id=req.params.id;
-    const post=Postmodel.findById(id);
-    
-    try{
-          const updatedPost=Postmodel.findByIdAndUpdate(
-            {_id:id},
-            {likeCount:post.likeCount+1}
-          )
-          res.status(201).json(newPost);
-
-    }catch(e){
-        res.status(500).json({message:e.message})
-
-    }
-
-}
-
-
+    res.status(201).json({ message: "comment added" });
+  } catch (e) {
+    console.log(e);
+    res.status(501).json(e);
+  }
+};
