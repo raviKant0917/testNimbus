27c1bf8a9dc9d282cedd88f2414330455f6c7a62
sanidhya99 <100680783+sanidhya99@users.noreply.github.com>
2023-03-10 10:00:55 +0530

    event updated

---
+++ controllers/eventControllers.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { event } from "../models/events";
+
+export const getEvents = async (request, response) => {
+  try {
+    const events = await event.find();
+    response.status(200).json(events);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+};
+export const postEvents = async (request, response) => {
+  const event = req.body;
+  const newEvent = new event(event);
+
+  try {
+    await newPost.save();
+    res.status(201).json(newEvent);
+  } catch (error) {
+    response.status(500).json(error);
+  }
+};
