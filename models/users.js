f57e849a5fad447da55c56ce3e6f6ca675dbd7e0
ravikant0917 <21bcs108@nith.ac.in>
2023-03-13 17:53:52 +0530

    user properties made required

--- models/users.js
+++ models/users.js
@@ -1,75 +1,83 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-    firebaseId: {
-        type: String,
-        required: true
+const userSchema = new mongoose.Schema(
+    {
+        firebaseId: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        userName: {
+            type: String,
+            minLength: 5,
+            maxLength: 128,
+            required: true,
+            unique: true,
+        },
+        phoneNumber: {
+            type: Number,
+            min: 6000000000,
+            max: 9999999999,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        fullName: {
+            type: String,
+            minLength: 2,
+            maxLength: 255,
+            required: true,
+        },
+        omegeleReports: {
+            type: Number,
+            default: 0,
+        },
+        OmegleAllowed: {
+            type: Boolean,
+            default: true,
+        },
+        favTeamVote: {
+            type: Boolean,
+            default: false,
+        },
+        instaId: {
+            type: String,
+            required: true,
+            default: "",
+            maxLength: 250,
+        },
+        profileImage: {
+            type: String,
+            maxLength: 255,
+            default: "",
+            required: true,
+        },
+        totalScore: {
+            type: Number,
+            default: 0,
+        },
+        linkedin: {
+            default: "",
+            type: String,
+            maxLength: 255,
+            required: true,
+        },
+        github: {
+            type: String,
+            default: "",
+            maxLength: 255,
+            required: true,
+        },
     },
-    userName: {
-        type: String,
-        minLength: 5,
-        maxLength: 128,
-        required: true,
-        unique: true
-    },
-    phoneNumber: {
-        type: Number,
-        min: 6000000000,
-        max: 9999999999,
-        null: false,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    fullName: {
-        type: String,
-        minLength: 2,
-        maxLength: 255,
-        required: true
-    },
-    omegeleReports : {
-        type: Number,
-        default : 0
-    },
-    OmegleAllowed : {
-        type : Boolean,
-        default : true
-    },
-    favTeamVote: {
-        type: Boolean,
-        default: false
-    },
-    instaId: {
-        type: String,
-        null: true,
-        default: "",
-        maxLength: 250
-    },
-    profileImage: {
-        type: String,
-        maxLength: 255,
-        required: true
-    },
-    totalScore: {
-        type: Number,
-        default: 0
-    },
-    linkedin: {
-        type: String,
-        maxLength : 255
-    },
-    github: {
-        type: String,
-        maxLength: 255
-    }
-},{timestamps: true});
+    { timestamps: true }
+);
 
-export const User = mongoose.model("User",userSchema);
-
+export const User = mongoose.model("User", userSchema);
